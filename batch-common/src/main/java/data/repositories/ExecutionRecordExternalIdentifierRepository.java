aa9328dab09af3f916346b6f06c8d64274b36301
Simon Tzanakis <Simon.Tzanakis@europeana.eu>
2024-05-23 09:57:47 +0200

    Pageable instead of PageRequest as per required.

--- batch-common/src/main/java/data/repositories/ExecutionRecordExternalIdentifierRepository.java
+++ batch-common/src/main/java/data/repositories/ExecutionRecordExternalIdentifierRepository.java
@@ -3,12 +3,12 @@ package data.repositories;
 import data.entity.ExecutionRecordExternalIdentifier;
 import data.entity.ExecutionRecordIdentifier;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExecutionRecordExternalIdentifierRepository<T extends ExecutionRecordIdentifier> extends JpaRepository<ExecutionRecordExternalIdentifier, T> {
 
-    Page<ExecutionRecordExternalIdentifier> findAllByExecutionId(String executionId, PageRequest pageable);
+    Page<ExecutionRecordExternalIdentifier> findAllByExecutionId(String executionId, Pageable pageable);
     long countByDatasetIdAndExecutionId(String datasetId, String executionId);
 
 }
